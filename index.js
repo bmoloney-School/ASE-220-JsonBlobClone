194b418006fd06fbe12565d0cf89ff5e4dbe0aef
Brandon Moloney <bdav24@protonmail.com>
2022-04-03 14:14:05 -0400

    Add all jsonBlob functionality & request tests

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ const express = require("express");
 const app = express()
 const port = 3000
 const blobPath = 'public/blobs/'
-const multer = require('multer')
-const upload = multer({ dest: blobPath })
 const { v4: uuidv4 } = require('uuid');
 var fs = require('fs');
 
@@ -21,15 +19,51 @@ app.post('/api/jsonBlob', (req, res) => {
         console.log(err)
     })
 
-    res.append('Location', 'localhost:' + port + '/api/jsonBlob/' + fileName)
+    res.append('Location', 'http:/localhost:' + port + '/api/jsonBlob/' + fileName)
     res.body = req.body
     res.send();
 });
 
 
+app.put('/api/jsonBlob/:blobId', (req, res) => {
+    let blobId = req.params.blobId;
+    let fileName = blobPath + blobId + '.json'
+    try {
+        let contents = fs.readFileSync(fileName);
+        fs.writeFile(fileName, JSON.stringify(req.body), function (err) {
+            console.log(err)
+        })
+        res.send(req.body)
+    }
+    catch (e) {
+        res.statusCode = 404;
+        res.send({ message: 'Blob does not exist' });
+    }
+});
 
+app.get('/api/jsonBlob/:blobId', (req, res) => {
+    let blobId = req.params.blobId;
 
+    try {
+        let contents = fs.readFileSync(blobPath + blobId + '.json');
+        res.send(contents);
+    }
+    catch (e) {
+        res.send({ message: 'Blob does not exist' })
+    }
+});
 
+app.delete('/api/jsonBlob/:blobId', (req, res) => {
+    let blobId = req.params.blobId;
+
+    try {
+        let contents = fs.unlinkSync(blobPath + blobId + '.json');
+        res.send({ message: 'Success' })
+    }
+    catch (e) {
+        res.send({ message: 'Blob does not exist' })
+    }
+});
 
 
 app.listen(port, () => {
